9b27d8d13d7ae6c0b460ea06ff63b76b6ffe6ecf
Ethan Neff <ethan.neff@eneff.com>
2017-10-17 12:07:06 -0700

    fix: make Linkedin deep linking work with Facebook, Pinterest, Branch, etc.

--- src/ios/AppDelegate+LinkedIn.m
+++ src/ios/AppDelegate+LinkedIn.m
@@ -6,6 +6,9 @@
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
     if ([LISDKCallbackHandler shouldHandleUrl:url]) {
         return [LISDKCallbackHandler application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+    } else {
+        // do other deep link routing for the Facebook SDK, Pinterest SDK, etc
+        [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLNotification object:url]];
     }
     return YES;
 }
